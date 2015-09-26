bacc43601a8daf1b0af114722d4775805d891d48
marvinick <marviluw@gmail.com>
2015-09-26 14:38:12 -0700

    debug directive

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -30,3 +30,17 @@ courseRoster.directive("alertUser", function() {
               '<div class="button" ng-click="dial({message:value})">Alert!</div>'
   }
 })
+
+courseRoster.directive("debug", function ($compile) {
+  return {
+    terminal: true,
+    priority: 1000000,
+    link: function (scope, element) {
+      var clone = element.clone();
+      element.attr("style", "color:red");
+      clone.removeAttr("debug");
+      var clonedElement = $compile(clone)(scope);
+      element.after(clonedElement);
+    }
+  }
+})
