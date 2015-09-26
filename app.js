7564b253401590375ebb8b743b9904a8aa993af6
marvinick <marviluw@gmail.com>
2015-09-25 20:01:05 -0700

    multiple views -- app.js

--- app.js
+++ app.js
@@ -3,18 +3,39 @@
  courseRoster.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
     url: "",
-    templateUrl: 'partials/home.html'
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: 'partials/home.html',
+      },
+    }
   });
 
   $stateProvider.state('courses', {
     url: "/courses",
-    templateUrl: "partials/courses.html",
-    controller: 'CoursesCtrl'
+    views: {
+      'header': {
+        templateUrl: 'partials/header',
+      },
+      'body': {
+        templateUrl: "partials/courses.html",
+        controller: 'CoursesCtrl'
+      },
+    }
   });
 
   $stateProvider.state('courses.students', {
     url: "/:courseId",
-    templateUrl: "partials/courses.students.html",
-    controller: 'StudentsCtrl'
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/courses.students.html",
+        controller: 'StudentsCtrl'
+      }
+    }
   });
  });
